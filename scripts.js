ca30a3064ff816a13441baf09834430102a9caea
FelipeFontouraBr <felipefontouramec@gmail.com>
2021-07-16 19:15:12 +0200

    Added object 'transition' and variable Transaction

--- scripts.js
+++ scripts.js
@@ -1,8 +1,44 @@
+// == MODAL ==
 const Modal = {
     open() {
         document.querySelector('.modal-overlay').classList.add('active')
     },
     close() {
         document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+
+// == Object (vai guardar as informações)
+const transitions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/07/2021'
+    },
+    {
+        id: 2,
+        description: 'Criação de website',
+        amount: 500000,
+        date: '23/07/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: 20000,
+        date: '23/07/2021'
+    },   
+    ]
+
+// == Transaction ==
+const Transaction = {
+    incomes() {
+        // Somar as entradas
+    },
+    expenses() {
+        // Somar as saídas
+    },
+    total() {
+        // Entradas - saídas
     }
 }
